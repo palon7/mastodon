5a5f3ea6470f696ee0a863d86abfafc9b8ceb259
Palon <palon7@gmail.com>
2017-05-13 00:43:43 +0900

    Tweak sidekiq settings, Added function to administration script

--- task.sh
+++ task.sh
@@ -8,18 +8,36 @@ if [ $# -lt 1 ]; then
   echo " compile-assets         : Precompile assets."
   echo " hot-restart            : Restart web server. (Hot restart)"
   echo " migrate                : Migrate Database."
+  echo " daily                  : Run daily task."
+  echo " login <instance>       : Login to instance."
+  echo " remove-remote          : Remove old media cache."
   echo ""
   exit 1
 fi
 
 # FIXME: Webサーバのスケールに対応できない
 # TODO: ちゃんと関数にする
+if [ $1 == "login" ]; then
+  sudo docker exec -it $2 /bin/sh
+fi
+
 if [ $1 == "hot-restart" ]; then
   sudo docker exec -it mastodon_web_1 kill -SIGUSR2 1
 fi
 
 if [ $1 == "compile-assets" ]; then
   sudo docker exec -it mastodon_web_1 rake assets:precompile
+fi
+
+if [ $1 == "confirm-email" ]; then
+  sudo docker exec -it mastodon_web_1 rake mastodon:confirm_email USER_EMAIL=$2
+fi
+if [ $1 == "daily" ]; then
+  sudo docker exec -it mastodon_web_1 rake mastodon:daily
+fi
+
+if [ $1 == "remove-remote" ]; then
+  sudo docker exec -it mastodon_web_1 rake mastodon:media:remove_remote
 fi
 
 if [ $1 == "migrate" ]; then
